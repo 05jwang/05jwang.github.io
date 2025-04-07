f38efad308c8003ee2277e90e3e2e604e2295c86
Jerry Wang <05jwang@gmail.com>
2025-04-07 16:11:16 -0400

    fix: fixed project page

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,18 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { ProjectsSectionContent } from '../components/sections/ProjectsSectionContent';
+import { mobileState } from '../recoil/atoms/mobileState';
 import { useRecoilState } from 'recoil';
 import { themeState } from '../recoil/atoms/themeState';
 
 const ProjectsPage: React.FC = () => {
   const ref = useRef<HTMLDivElement>(null);
   const [darkTheme] = useRecoilState(themeState);
+  const [isMobile, setIsMobile] = useRecoilState(mobileState);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 700);
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div
       style={{
         marginTop: '50px',
+        maxWidth: '95%',
       }}
     >
-      <ProjectsSectionContent theme={darkTheme} />
+      <ProjectsSectionContent theme={darkTheme} isMobile={isMobile} />
     </div>
   );
 };
